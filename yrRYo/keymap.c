39eaabac031686e0b183bc23c83821b94f8fc223
Curtis Vogt <curtis.vogt@gmail.com>
2024-12-12 22:29:28 -0600

    Possibly need to NULL terminate key overrides
    Official documentation doesn't mention this but it appears to have
    worked for others:
    
    - https://docs.qmk.fm/features/key_overrides
    - https://www.reddit.com/r/olkb/comments/s0lmt8/qmk_key_overrides_hold_overridden_mod_to_keep_the/
    - https://github.com/jcc03/oryx-with-custom-qmk/commit/25b340facdc4ae293e2d0e5d1bd48667cfb788e1

--- yrRYo/keymap.c
+++ yrRYo/keymap.c
@@ -103,5 +103,6 @@ const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC
 
 // Globally define all key overrides
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
 };
