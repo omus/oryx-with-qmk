51b97a2fec43470b538edcccac24659b0852cb75
Curtis Vogt <curtis.vogt@gmail.com>
2025-01-10 15:49:38 -0600

    Tap-hold configuration for home row mods

--- yrRYo/config.h
+++ yrRYo/config.h
@@ -3,11 +3,14 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define PERMISSIVE_HOLD
-
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define SERIAL_NUMBER "yrRYo/4LqLo"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
 
+// Tap-hold configuration for home row mods.
+// https://getreuer.info/posts/keyboards/achordion/index.html#tap-hold-configuration
+#define TAPPING_TERM 175
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM_PER_KEY
